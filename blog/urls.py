344a1ab7c05e85f941943e74328b6324acf506fb
psachin <iclcoolster@gmail.com>
2014-11-18 13:03:54 +0530

    Subject: Edit post.
    Description:
    
    TODO:
    
    - Publish post

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_details),
     url(r'^post/new/$', views.post_new),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 )
